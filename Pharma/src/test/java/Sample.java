f9f954a6728b833693c2e44af73f4bba35a7b36a
swapnawalmart <gollapudiswapna@gmail.com>
2022-02-05 20:37:27 +0530

    added code

---
+++ Pharma/src/test/java/Sample.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+    public void doTest1(){
+        System.out.println("Hello from Test 1");
+    }
+    @Test
+    public void doTest2(){
+        System.out.println("Hello from Test 2");
+    }
+    @Test
+    public void doTest3(){
+        System.out.println("Hello from Test 3");
+    }
+    @Test
+    public void doTest4(){
+        System.out.println("Hello from Test 4");
+    }
+    @Test
+    public void doTest5(){
+        System.out.println("Hello from Test 5");
+    }    
+
+}
